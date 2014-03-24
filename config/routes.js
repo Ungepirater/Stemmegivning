00222e016a1732a8007a0cd072ceee7cbcfa04b7
skeldoy <skeldoy@gmail.com>
2014-03-24 23:08:35 +0100

    * Port existing stuff from express to sails. * Overload two out of four blueprint controller-functions on the guest controller - the other two must be fixed * Simplified email-verification algo by not going back and forth from base64 - maybe there was a point to it, that is now gone.

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,11 @@ module.exports.routes = {
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
     view: 'home/index'
-  }
+  },
+
+  '/vote': {
+    view: 'home/vote'
+}
 
   /*
   // But what if you want your home page to display
